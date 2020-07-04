eb5a7415f920cfe1eac6bcce891f1437c6d457ce
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-07-04 12:51:30 +0300

    addition of the fifth task

--- js/task5.js
+++ js/task5.js
@@ -1,41 +1,45 @@
 'use strict';
 
 const userInput = prompt('Укажите страну доставки пожалуйста!');
-let normalizedInput = userInput.toLowerCase();
 let price;
 
-switch (normalizedInput) {
+if (userInput === null) {
+  console.log('Отменено пользователем!');
+} else {
+  let normalizedInput = userInput.toLowerCase();
 
-        case 'китай':
-          price = 100;
-          normalizedInput = "Китай";
-          console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
-          break;
+  switch (normalizedInput) {
+    case 'китай':
+      price = 100;
+      normalizedInput = "Китай";
+      console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
+      break;
+
+    case 'чили':
+      price = 250;
+      normalizedInput = "Чили";
+      console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
+      break;
     
-        case 'чили':
-          price = 250;
-          normalizedInput = "Чили";
-          console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
-          break;
-        
-        case 'австралия':
-          price = 170;
-          normalizedInput = "Австаралию";
-          console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
-          break;
-    
-        case 'индия':
-          price = 80;
-          normalizedInput = "Индию";
-          console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
-          break;
-    
-        case 'ямайка':
-          price = 120;
-          normalizedInput = "Ямайку";
-          console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
-          break;
-    
-        default:
-        alert('В вашей стране доставка не доступна');
-    }
+    case 'австралия':
+      price = 170;
+      normalizedInput = "Австаралию";
+      console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
+      break;
+
+    case 'индия':
+      price = 80;
+      normalizedInput = "Индию";
+      console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
+      break;
+
+    case 'ямайка':
+      price = 120;
+      normalizedInput = "Ямайку";
+      console.log(`Доставка в ${normalizedInput} будет стоить ${price} кредитов.`);
+      break;
+
+    default:
+    alert('В вашей стране доставка не доступна');
+}
+}
